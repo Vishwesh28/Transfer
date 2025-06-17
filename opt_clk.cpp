7a972972665ec14e36261df6d9f349a13c66656a
Vishwesh28 <90981500+Vishwesh28@users.noreply.github.com>
2025-06-17 16:06:18 +0530

    Update opt_clk.cpp

--- opt_clk.cpp
+++ opt_clk.cpp
@@ -1,16 +1,21 @@
 #include <csignal>
 #include <iostream>
+using namespace std;
 
 volatile bool keep_running = true;
+
+chrono::high_resolution_clock::time_point start_time;
+uint64_t tick_count = 0;
 
 void handle_sigint(int) {
     keep_running = false;
 }
 
 int main() {
-    std::signal(SIGINT, handle_sigint);
+    signal(SIGINT, handle_sigint);
 
     uint64_t ticks = 0;
+    start_time = chrono::high_resolution_clock::now();
 
     asm volatile (
         "mov %[ticks], %%rax\n\t"    // Move ticks to RAX
@@ -24,6 +29,14 @@ int main() {
         : "rax"
     );
 
-    std::cout << "Ticks: " << ticks << std::endl;
+    auto end_time = chrono::high_resolution_clock::now();
+    auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count();
+    double seconds = elapsed_ms / 1000.0;
+
+    cout << "\n--- Tick Simulation Ended ---\n";
+    cout << "Total Ticks: " << ticks << "\n";
+    cout << "Elapsed Time: " << seconds << " sec\n";
+    cout << "Tick Rate: " << (ticks / seconds) << " ticks/sec\n";
+    cout << "How many times quicker than a jifyy: " << ((ticks / seconds)/65536) << endl;
     return 0;
 }
